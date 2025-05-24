ddf756b6efdd0b1c8c928454d36d8a7ecde1d0e1
Shiven <shivendashora@originmedical.ai>
2025-05-24 14:39:23 +0530

    selecting logic added

--- nextjs-demo/src/app/Components/mediagrid.tsx
+++ nextjs-demo/src/app/Components/mediagrid.tsx
@@ -1,42 +1,51 @@
-import Image from "next/image"
-import styles from '../mediagrid.module.css'
+import Image from "next/image";
+import styles from "../mediagrid.module.css";
 import { useState } from "react";
-interface mediaprops {
-    stateshare: boolean;
-}
-export default function Media({ stateshare }: mediaprops) {
-    let imagepath: Array<string> = ["/images.jpeg", "/images.jpeg", "/images.jpeg", "/images.jpeg", "/images.jpeg", "/images.jpeg"];
-    let [selectedImages,setselectedImages] = useState<Array<string>>([]);
-    let [color,setcolor] = useState<Boolean>(false);
-    function colored(){
-        setcolor(true);
-    }
-    /*function selected(index:number):[]{
-        setselectedImages();
-        return selectedImages;
 
-    }*/
-   {console.log(color)}
-    return (
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-                <div className={styles.grid}>
-                    {imagepath.map((path, index) => (
-                        <div className={styles.select} key={index}>
-                            <Image
-                                src={path}
-                                alt={`Image ${index}`}
-                                height={340}
-                                width={450}
-                                className={styles.image}
-                                onClick={colored}
-    
-                            />
-                            {stateshare && (
-                                <div className={color ? styles.blank : styles.selectButton}></div>
-                            )}
-                        </div>
-                    ))}
-                </div>
-            </div>
-    )
+interface MediaProps {
+  stateshare: boolean;
+}
+
+export default function Media({ stateshare }: MediaProps) {
+  const imagePaths: string[] = Array(6).fill("/images.jpeg");
+
+  const [selectedIndexes, setSelectedIndexes] = useState<Set<number>>(new Set());
+
+  const toggleSelect = (index: number) => {
+    setSelectedIndexes((prev) => {
+      const newSet = new Set(prev);
+      if (newSet.has(index)) {
+        newSet.delete(index);
+      } else {
+        newSet.add(index);
+      }
+      return newSet;
+    });
+  };
+
+  return (
+    <div className="mainContainer">
+      <div className={styles.grid}>
+        {imagePaths.map((path, index) => (
+          <div className={styles.select} key={index}>
+            <Image
+              src={path}
+              alt={`Image ${index}`}
+              height={340}
+              width={450}
+              className={styles.image}
+              onClick={() => toggleSelect(index)}
+            />
+            {stateshare && (
+              <div
+                className={`${styles.selectButton} ${
+                  selectedIndexes.has(index) ? styles.selected : ""
+                }`}
+              ></div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
